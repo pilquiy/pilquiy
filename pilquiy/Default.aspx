a6d5527decf88cc6900efaeccb695bd6e475b921
pilquiy <pilquiy@yahoo.co.uk>
2012-09-13 10:57:32 +0100

    added a web solution

---
+++ pilquiy/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="pilquiy" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="pilquiy._Default" %>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="BodyContent">
+  <p>Lorem ipsum...</p> 
+    <p><input type="number" id="num" /><input type="button" value="get" onclick="btnGet()" /></p>
+</asp:Content>
---
+++ pilquiy/Scripts/pilquiy.js
@@ -0,0 +1,21 @@
+﻿/* filename: pilquiy.js */
+/******************************************************************************************
+
+All the code and functionality of the pilquiy application
+
+******************************************************************************************/
+
+pilquiy.Pilquiy.GetRandomData(13, _getRandomData_OK, _getRandomDate_FAIL, "userContext");
+
+function _getRandomData_OK(payload){
+    alert(payload + " :-)");
+}
+
+function _getRandomDate_FAIL(err){
+    alert("!!" + err + " :-(");
+}
+
+function btnGet() {
+    pilquiy.Pilquiy.GetRandomData($('#num').val(), _getRandomData_OK, _getRandomDate_FAIL, "userContext");
+}
+

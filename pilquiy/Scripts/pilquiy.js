b0269e57e54546af636c4d90d6a7ccb6b6fda7ed
pilquiy <pilquiy@yahoo.co.uk>
2012-09-13 11:07:34 +0100

    removed the onLoad function call

--- pilquiy/Scripts/pilquiy.js
+++ pilquiy/Scripts/pilquiy.js
@@ -5,8 +5,6 @@ All the code and functionality of the pilquiy application
 
 ******************************************************************************************/
 
-pilquiy.Pilquiy.GetRandomData(13, _getRandomData_OK, _getRandomDate_FAIL, "userContext");
-
 function _getRandomData_OK(payload){
     alert(payload + " :-)");
 }
